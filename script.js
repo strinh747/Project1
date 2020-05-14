a190563f66f3c69423b7ff4487b6826e4ad0d10b
wizeapps <jrgsolis@gmail.com>
2020-05-14 02:43:15 -0400

    Third API Call Full Recipe Description

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     var foodIngredients = [];
+    var cocktailIngredients = [];
 
     $("#food-add-button").on("click", function(event) {
         event.preventDefault();
@@ -15,11 +16,37 @@ $(document).ready(function() {
         searchRecipies(foodIngredients);
     })
 
+    $("#cocktail-add-button").on("click", function(event) {
+      event.preventDefault();
+      var ingredient = $("#cocktail-search-input").val();
+      cocktailIngredients.push(ingredient);
+      if (cocktailIngredients.length > 1) {
+          cocktailIngredients = cocktailIngredients.slice(1,2);
+      }
+      // localStorage.setItem("cocktail-ingredients", JSON.stringify(cocktailIngredients));
+      $("#cocktail-search-input").val("");
+      renderCocktailIngredients();
+      searchCocktails(cocktailIngredients);
+  })
+
+
     $(document).on("click", ".callout", function(){
         foodIngredients.splice( foodIngredients.indexOf($(this).find("button").attr("ingredient-data")), 1 )
         searchRecipies(foodIngredients);
     }
     )
+
+    $(document).on("click", ".callout", function(){
+      cocktailIngredients.splice(cocktailIngredients.indexOf($(this).find("button").attr("cocktail-data")), 1 )
+      searchCocktails(cocktailIngredients);
+  }
+  )
+
+  $(document).on("click", ".card", function(){
+    console.log($(this).attr("id"))
+    getCocktailRecipe($(this).attr("id"));
+}
+)
 
     function renderFoodIngredients() {
         // var storedFoodIngredients = localStorage.getItem("food-ingredients");
@@ -37,6 +64,23 @@ $(document).ready(function() {
             $("#food-button-storage").append(div);
         }
     }
+
+    function renderCocktailIngredients() {
+      // var storedCocktailIngredients = localStorage.getItem("cocktail-ingredients");
+      // cocktailIngredients = JSON.parse(storedCocktailIngredients);
+      $("#cocktail-button-storage").empty()
+      for (i=0; i<cocktailIngredients.length; i++) {
+          var div = $('<div class="callout" data-closable>');
+          var button = $('<button class="close-button" aria-label="Close alert" type="button" data-close>');
+          var span = $('<span aria-hidden="true">&times;</span>');
+          var p = $("<p>");
+          button.append(span);
+          p.text(cocktailIngredients[i]);
+          button.attr("cocktail-data", cocktailIngredients[i]);
+          div.append(button,p);
+          $("#cocktail-button-storage").append(div);
+      }
+  }
 
     function searchRecipies(ingredients) {
 
@@ -103,6 +147,97 @@ $(document).ready(function() {
         });
       }
 
+    function searchCocktails(ingredients) {
+
+      var encodedIngredients =encodeURI(ingredients);
+  
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://the-cocktail-db.p.rapidapi.com/filter.php?i=" + encodedIngredients,
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+          "x-rapidapi-key": "fd013058a6msh0ecbe48adf23f07p1ebed4jsnb083bffefd39"
+        }
+      }
+      $.ajax(settings).done(function (response) {
+        if (response.drinks.length > 0) {
+          $("#cocktails-div").empty();
+          {
+            jQuery.each(response.drinks, function( i, item ) {
+              var divcell = $('<div class="cell">');
+              var divcard = $('<div class="card" id="' + item.idDrink + '">');
+              var divcardsec = $('<div class="card-section">');
+              var recipeName = $("<h3>").text(item.strDrink);
+              // var imageURL = $("<a>").attr("href", item.image).append(recipeName);
+              var Image = $("<img>").attr("src", item.strDrinkThumb);
+              
+              divcardsec.append(recipeName);
+              divcard.append(Image,divcardsec);
+              var a = $('<a href="#' + item.idDrink + 'modal" rel="modal:open">').html(divcard);
+              divcell.append(a);
+              $("#cocktails-div").append(divcell);
+              
+              
+
+              // $.ajax({
+              //     "async": true,
+              //     "crossDomain": true,
+              //     "url": "https://webknox-recipes.p.rapidapi.com/recipes/"+recipeId+"/summary/",
+              //     "method": "GET",
+              //     "headers": {
+              //       "x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+              //       "x-rapidapi-key": "JMI8oJ73JbFA1XPeI85oBxTgNRBNu0U5"
+              //     }
+              // }).done(function (response) {
+              //     console.log(response.summary)
+              //     var description = $("<p>").html(response.summary);
+              //     divcardsec.append(description);
+
+              //   return response.summary;
+                
+              // }); 
+
+            });
+          }
+
+        }
+        else {
+          // setResponse("Please provide me more ingredients.");
+        }
+  
+      });
+    }
+
+
+    function getCocktailRecipe(id) {
+      
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://the-cocktail-db.p.rapidapi.com/lookup.php?i=" + id,
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+          "x-rapidapi-key": "fd013058a6msh0ecbe48adf23f07p1ebed4jsnb083bffefd39"
+        }
+      }
+
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+
+        var div = $('<div class="modal">').attr("id", id + "modal");
+        var p = $('<p>');
+        var a = $('<a href="#" rel="modal:close">').text("Close");
+        p.text(JSON.stringify(response.drinks[0]));
+        div.append(p,a);
+        $("#cocktails-div").append(div);
+
+        
+      });
+
+    }
 })
 
 
